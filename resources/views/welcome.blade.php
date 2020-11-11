7c3d7dd3bbaa1f4f26c111097934006b82d5e569
Tenjomo <tenjomoherve@gmail.com>
2020-11-11 16:06:03 +0100

    affichage des sliders au niveau du frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,41 +16,27 @@
         <link rel="stylesheet" href="{{ asset('frontend/css/flexslider.css') }}">
         <link rel="stylesheet" href="{{ asset('frontend/css/pricing.css') }}">
         <link rel="stylesheet" href="{{ asset('frontend/css/main.css') }}">
+        <style>
+                @foreach($sliders as $key=>$slider)
 
+                    .owl-carousel .owl-wrapper, .owl-carousel .owl-item:nth-child({{ $key + 1 }}) .item
+                    {
+                        background: url({{ asset('uploads/slider/'.$slider->image) }});
+                        background-size: cover;
+                    }
+                @endforeach
+        </style>
 
-        <script src="{{ asset('frontend/js/jquery-1.11.2.min.js') }}"></script>
-        <script type="text/javascript" src="{{ asset('frontend/js/jquery.flexslider.min.js') }}"></script>
-        <script type="text/javascript">
-            $(window).load(function() {
-                $('.flexslider').flexslider({
-                 animation: "slide",
-                 controlsContainer: ".flexslider-container"
+            <script src="{{ asset('frontend/js/jquery-1.11.2.min.js') }}"></script>
+            <script type="text/javascript" src="{{ asset('frontend/js/jquery.flexslider.min.js') }}"></script>
+            <script type="text/javascript">
+                $(window).load(function() {
+                    $('.flexslider').flexslider({
+                        animation: "slide",
+                        controlsContainer: ".flexslider-container"
+                    });
                 });
-            });
-        </script>
-
-        <script src="https://maps.googleapis.com/maps/api/js"></script>
-        <script>
-            function initialize() {
-                var mapCanvas = document.getElementById('map-canvas');
-                var mapOptions = {
-                    center: new google.maps.LatLng(24.909439, 91.833800),
-                    zoom: 16,
-                    scrollwheel: false,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                }
-                var map = new google.maps.Map(mapCanvas, mapOptions)
-
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(24.909439, 91.833800),
-                    title:"Mamma's Kitchen Restaurant"
-                });
-
-                // To add the marker to the map, call setMap();
-                marker.setMap(map);
-            }
-            google.maps.event.addDomListener(window, 'load', initialize);
-        </script>
+            </script>
 
 
     </head>
@@ -90,30 +76,16 @@
 
         <!--== 5. Header ==-->
         <section id="header-slider" class="owl-carousel">
-            <div class="item">
-                <div class="container">
-                    <div class="header-content">
-                        <h1 class="header-title">BEST FOOD</h1>
-                        <p class="header-sub-title">create your own slogan</p>
-                    </div> <!-- /.header-content -->
+            @foreach($sliders as $key=>$slider)
+                <div class="item">
+                    <div class="container">
+                        <div class="header-content">
+                            <h1 class="header-title">{{ $slider->title }}</h1>
+                            <p class="header-sub-title">{{ $slider->sub_title }}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="container">
-                    <div class="header-content">
-                        <h1 class="header-title">BEST SNACKS</h1>
-                        <p class="header-sub-title">create your own slogan</p>
-                    </div> <!-- /.header-content -->
-                </div>
-            </div>
-            <div class="item">
-                <div class="container">
-                    <div class="header-content text-right pull-right">
-                        <h1 class="header-title">BEST DRINKS</h1>
-                        <p class="header-sub-title">create your own slogan</p>
-                    </div> <!-- /.header-content -->
-                </div>
-            </div>
+            @endforeach
         </section>
 
 
